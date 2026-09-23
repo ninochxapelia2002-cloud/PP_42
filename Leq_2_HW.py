e46d066badf07c558df82f7739dec37f9b500303
Nino Chkhaidze <ninochxapelia2002@gmail.com>
2026-09-23 11:52:06 +0400

    Updated Lecture 2 homework

--- Leq_2_HW.py
+++ Leq_2_HW.py
@@ -1,4 +1,5 @@
 from math import sqrt
+## 1
 Person_Name = "Nino Chkhapelia"
 Person_Age = 23
 Is_Employed = True
@@ -8,3 +9,26 @@ print(type(Person_Name))
 print(type(Person_Age))
 print(type(Is_Employed))
 print(type(love_number))
+
+## 2
+Birth_year = int(input("Enter your birth year: "))
+
+Age = 2026 - Birth_year
+
+print("Your approximate age is:", Age)
+
+
+## 3
+Number = int(input("Enter a number: "))
+
+if Number > 0:
+    print("Positive")
+elif Number < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+if Number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
