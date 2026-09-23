e239b423650ad5166077f5c9a8be72c3287032ca
Nino Chkhaidze <ninochxapelia2002@gmail.com>
2026-09-23 11:20:19 +0400

    Lecture 2 homework

---
+++ Leq_2_HW.py
@@ -0,0 +1,10 @@
+from math import sqrt
+Person_Name = "Nino Chkhapelia"
+Person_Age = 23
+Is_Employed = True
+love_number = sqrt(2)
+
+print(type(Person_Name))
+print(type(Person_Age))
+print(type(Is_Employed))
+print(type(love_number))
